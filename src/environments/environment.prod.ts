995f20fbd9aff76a0f1159a2e8e30feb50d9afab
Yosmi Gutierrez <yosangut06@gmail.com>
2023-10-05 13:31:21 -0600

    Evaluación

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: '006157e14ab34fb1b76ada3a4ac0c900'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '006157e14ab34fb1b76ada3a4ac0c900'
 };
 
 /*
